c185497f6d6feb776fc39fcc4f56fa27b2e25743
Rafael Machado <rafaelxw7@gmail.com>
2021-06-16 17:28:35 -0300

    Aula 05 - Strings e Arrays

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,47 @@
+// Exercícios de interpretação de código
+
+// 1)
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. 3 19 5 6 7 8 9 10 11 12 13
+// f. 8
+
+// 2)
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+//  Exercícios de escrita de código
+
+//    >>>>>   Exercício 1
+
+// const nomeDoUsuario = prompt('Digite seu nome')
+// const emailDoUsuario = prompt('Digite seu e-mail')
+//     console.log('O e-mail', emailDoUsuario, 'foi cadastrado com sucesso. Seja bem-vinda(o),', nomeDoUsuario,'!')
+
+//     >>>>>   Exercício 2
+
+const comidasFav = ['lasanha', 'pão com ovo', 'pizza', 'cheeseburger', 'espaguete']
+
+// a)
+console.log(comidasFav)
+// b
+console.log('Essas são as minhas comidas preferidas: ')
+console.log(comidasFav[0])
+console.log(comidasFav[1])
+console.log(comidasFav[2])
+console.log(comidasFav[3])
+console.log(comidasFav[4])
+
+// c
+const comidaUsuario = prompt("Sr. Stark digite sua comida preferida:")
+
+comidasFav[1] = comidaUsuario
+
+console.log(comidasFav)
+
+
+
+
+
+
