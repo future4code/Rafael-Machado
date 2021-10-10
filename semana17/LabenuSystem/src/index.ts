ac0bc90f48459be8dea95e1509711b7e2bce69c1
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-10-10 11:08:08 -0300

    Endpoint para criar estudante OK

--- semana17/LabenuSystem/src/index.ts
+++ semana17/LabenuSystem/src/index.ts
@@ -2,16 +2,7 @@ import app from './app'
 import { Request, Response } from 'express'
 import connection from './connection'
 
-
-// app.post("/user", createUser)
-
-// app.get("/user/:id", getUserById)
-
-// app.post("/user/edit/:id", editUser)
-
-// app.put("/task", createTask)
-
-// app.get("/task/:id", getTaskById)
+// Vídeo >>> 38m
 
 enum TIPO_TURMA {
    INTEGRAL = "integral",
@@ -27,6 +18,14 @@ type criaTurmaInput = {
    tipo: TIPO_TURMA
 }
 
+type criaEstudanteInput = {
+   id: number,
+   nome: string,
+   email: string,
+   data_nasc: string,
+   hobbies: string[],
+   turma_id: number
+}
 
 app.post("/turma", async (req: Request, res: Response) => {
    let errorCode = 400
@@ -70,6 +69,65 @@ app.post("/turma", async (req: Request, res: Response) => {
       
    } catch (error: any) {
       res.status(errorCode).send({ message: error.message })
+   }
+
+})
+
+app.post("/estudante", async (req: Request, res: Response) => {
+   let errorCode = 400
+
+   try {
+      const input: criaEstudanteInput = {
+         id: req.body.id,
+         nome: req.body.nome,
+         email: req.body.email,
+         data_nasc: req.body.data_nasc,
+         hobbies: req.body.hobbies,
+         turma_id: req.body.turma_id
+      }
+
+      if (!input.id || !input.nome || !input.email || !input.data_nasc 
+         || input.hobbies.length < 1 ) {
+         errorCode = 422
+         throw new Error("Preencha os campos corretamente")
+      }
+
+      await connection.raw(`
+         INSERT INTO ESTUDANTE (id, nome, email, data_nasc, turma_id)
+         VALUES
+         (   ${input.id},
+            "${input.nome}",
+            "${input.email}",
+            "${input.data_nasc}",
+             ${input.turma_id}
+         )
+      `)
+
+      for (let hobby of input.hobbies) {
+         const idPassatempo = Math.floor(Math.random() * 1000000)
+         await connection.raw(`
+         INSERT INTO PASSATEMPO (id, nome)
+         VALUES(
+            ${idPassatempo},
+            "${hobby}"
+         )
+         `)
+
+         await connection.raw(`
+         INSERT INTO ESTUDANTE_PASSATEMPO (estudante_id, passatempo_id)
+         VALUES(
+            ${input.id},
+            ${idPassatempo}
+         )
+         `)
+      }
+
+      res.status(201).send({ message: "Estudante gravado com sucesso!"})
+
+      
+   } catch (error: any) {
+      res.status(errorCode).send({ message: error.message })
+      
    }
 
 })
