0bf995fbc91acb8c5bc5753c879165d7fef09b40
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-10-25 14:31:05 -0300

    Criado Endpoint GetUserById

--- semana19/projeto-cookenu/src/index.ts
+++ semana19/projeto-cookenu/src/index.ts
@@ -1,5 +1,6 @@
 import app from './app'
 import getProfile from './endpoints/users/getProfile'
+import getUserById from './endpoints/users/getUserById'
 import login from './endpoints/users/login'
 import signup from './endpoints/users/signup'
 
@@ -7,7 +8,7 @@ import signup from './endpoints/users/signup'
 app.post("/users/signup", signup)
 app.post("/users/login", login)
 app.get("/users/profile", getProfile)
-app.get("/users/:id/profile")
+app.get("/users/:id/profile", getUserById)
 
 app.post("/recipe")
 app.get("/recipe/:id")
