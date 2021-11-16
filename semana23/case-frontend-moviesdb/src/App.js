311b355b9e3749112bf7eff79a6e589a08dd1caf
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-11-16 18:28:49 -0300

    Configurações iniciais e estilo global

--- semana23/case-frontend-moviesdb/src/App.js
+++ semana23/case-frontend-moviesdb/src/App.js
@@ -1,9 +1,16 @@
+import Movie from "./components/Movie"
 
 
 function App() {
+
+const movies = ["1", "2", "3"]
+
+
   return (
     <div>
-      Hello Movies App
+      {movies.map(movie => (
+        <Movie />
+      ))}
     </div>
   )
 }
---
+++ semana23/case-frontend-moviesdb/src/components/Movie.js
@@ -0,0 +1,11 @@
+
+
+const Movie = () => {
+    return (
+        <div className="movie">
+            Componente - Filmes
+        </div>
+    )
+}
+
+export default Movie
