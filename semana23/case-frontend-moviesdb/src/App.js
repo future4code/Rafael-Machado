bbad693c9a5af1aef57796d1abec0d68c518b7f7
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-11-17 20:08:22 -0300

    Ajustes Inicio do Projeto

--- semana23/case-frontend-moviesdb/src/App.js
+++ semana23/case-frontend-moviesdb/src/App.js
@@ -1,18 +1,11 @@
-import Movie from "./components/Movie"
 
 
 function App() {
-
-const movies = ["1", "2", "3"]
-
-
   return (
     <div>
-      {movies.map(movie => (
-        <Movie />
-      ))}
+      MOVIES APP
     </div>
   )
 }
 
-export default App
+export default App;
